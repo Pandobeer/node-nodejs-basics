283b561451520f1fdb8329a36ea483d744f059fe
Pandobeer <nikolyav@gmail.com>
2022-12-02 00:34:27 +0100

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+
+
 const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
+    fs.writeFile(filePath, 'I am fresh and young', { flag: 'wx' }).catch((err) => {
+        console.log(err);
+        throw new Error('FS operation failed');
+    });
 };
 
 await create();
