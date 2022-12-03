a58e03a9461d9c3fe23d39b8336d43db4c46eece
Pandobeer <nikolyav@gmail.com>
2022-12-03 14:19:08 +0100

    feat: add cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
-const parseEnv = () => {
-    // Write your code here 
+import { env } from 'node:process';
+
+const parseEnv = async () => {
+    const keys = Object.keys(env);
+    const filteredKeys = keys.filter((key) => key.startsWith('RSS_'));
+
+    const result = filteredKeys.reduce((acc, key, index) => {
+        if (index === (filteredKeys.length - 1)) {
+            acc += `${key}=${env[key]}`;
+        } else {
+            acc += `${key}=${env[key]}; `;
+
+        }
+        return acc;
+    }, '');
+    console.log(result);
 };
 
-parseEnv();
+await parseEnv();
