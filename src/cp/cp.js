15093672827a8d68a1253a2843b36de905cd9fff
Pandobeer <nikolyav@gmail.com>
2022-12-04 21:55:27 +0100

    feat: add spawn task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import { fork } from 'node:child_process';
+
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'script.js');
+
+    fork(filePath, args);
+
 };
 
-spawnChildProcess();
+await spawnChildProcess(['arg1', 'arg2', 'arg3']);
