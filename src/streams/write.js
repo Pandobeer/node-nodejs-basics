c4c70243f5994f70e3261067356c9b73b0c62943
Pandobeer <nikolyav@gmail.com>
2022-12-04 15:20:25 +0100

    feat: add read, write, transform streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs';
+import process from 'node:process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    const writeStream = fs.createWriteStream(filePath);
+
+    process.stdin.pipe(writeStream);
+
+    writeStream.on('error', (err) => {
+        console.error(err);
+        process.exitCode = 1;
+    });
 };
 
 await write();
