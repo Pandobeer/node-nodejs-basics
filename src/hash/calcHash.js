cc0f456a4c8558b60e976c86df247be379ffa2d9
Pandobeer <nikolyav@gmail.com>
2022-12-05 14:25:23 +0100

    fix: fix error in calc hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,8 +1,19 @@
 import { createHash } from 'node:crypto';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
 
 const calculateHash = async () => {
-    const hex = createHash('sha256').update('fileToCalculateHasFor.txt').digest('hex');
-    console.log(hex);
+    try {
+        const hash = createHash('sha256').update(filePath).digest('hex');
+        console.log(hash);
+    } catch (err) {
+        console.error(err);
+    }
+
 };
 
 await calculateHash();
